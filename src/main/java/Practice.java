934435f485c7e7e7170a2ab327c7604d13876fd0
darjadovbnja <darja.dovbnja@gmail.com>
2020-04-19 19:24:38 -0700

    Committed Practice

---
+++ src/main/java/Practice.java
@@ -0,0 +1,20 @@
+
+public class Practice {
+    String breed;
+    int age;
+    String color;
+    void barking () {
+    }
+    void eat () {
+    }
+    void sleep(){
+    }
+
+    public static void main (String[] args){
+        System.out.println("Hello my dudes");
+    }
+}
+
+
+
+
